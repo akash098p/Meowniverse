7562e7ef455f963a70464f021a0dd03229b1aaa9
Akash Pramanik <akashpramanik422@gmail.com>
2026-07-20 15:34:28 +0530

    Add food and toys

---
+++ src/config/items/foods.js
@@ -0,0 +1,16 @@
+export default {
+    fish: { id: 'fish', name: 'Fish', emoji: '🐟', cost: 10, hunger: 30, happiness: 5, energy: 5, rarity: 'common', unlockLevel: 1 },
+    bone: { id: 'bone', name: 'Bone', emoji: '🦴', cost: 10, hunger: 25, happiness: 10, energy: 5, rarity: 'common', unlockLevel: 1 },
+    carrot: { id: 'carrot', name: 'Carrot', emoji: '🥕', cost: 8, hunger: 20, happiness: 5, energy: 10, rarity: 'common', unlockLevel: 1 },
+    bread: { id: 'bread', name: 'Bread', emoji: '🍞', cost: 5, hunger: 15, happiness: 3, energy: 5, rarity: 'common', unlockLevel: 1 },
+    berry: { id: 'berry', name: 'Berry', emoji: '🫐', cost: 12, hunger: 15, happiness: 15, energy: 10, rarity: 'common', unlockLevel: 2 },
+    steak: { id: 'steak', name: 'Steak', emoji: '🥩', cost: 30, hunger: 50, happiness: 10, energy: 20, strength: 5, rarity: 'uncommon', unlockLevel: 5 },
+    cake: { id: 'cake', name: 'Cake', emoji: '🎂', cost: 25, hunger: 20, happiness: 40, energy: 10, rarity: 'uncommon', unlockLevel: 3 },
+    pizza: { id: 'pizza', name: 'Pizza', emoji: '🍕', cost: 20, hunger: 35, happiness: 20, energy: 15, rarity: 'common', unlockLevel: 2 },
+    iceCream: { id: 'iceCream', name: 'Ice Cream', emoji: '🍦', cost: 15, hunger: 10, happiness: 30, energy: 5, rarity: 'common', unlockLevel: 1 },
+    sushi: { id: 'sushi', name: 'Sushi', emoji: '🍣', cost: 35, hunger: 30, happiness: 20, energy: 15, intelligence: 5, rarity: 'uncommon', unlockLevel: 6 },
+    salad: { id: 'salad', name: 'Salad', emoji: '🥗', cost: 12, hunger: 20, happiness: 5, energy: 15, cleanliness: 10, rarity: 'common', unlockLevel: 2 },
+    goldenApple: { id: 'goldenApple', name: 'Golden Apple', emoji: '🍎', cost: 100, hunger: 50, happiness: 50, energy: 50, health: 30, rarity: 'rare', unlockLevel: 10 },
+    magicPotion: { id: 'magicPotion', name: 'Magic Potion', emoji: '🧪', cost: 200, hunger: 0, happiness: 0, energy: 100, health: 50, intelligence: 20, rarity: 'rare', unlockLevel: 15 },
+    rainbowCandy: { id: 'rainbowCandy', name: 'Rainbow Candy', emoji: '🍬', cost: 50, hunger: 5, happiness: 60, energy: 20, rarity: 'uncommon', unlockLevel: 8 }
+};
---
+++ src/config/items/toys.js
@@ -0,0 +1,12 @@
+export default {
+    ball: { id: 'ball', name: 'Ball', emoji: '⚽', cost: 15, happiness: 20, energy: -10, rarity: 'common', unlockLevel: 1 },
+    yarn: { id: 'yarn', name: 'Yarn Ball', emoji: '🧶', cost: 12, happiness: 25, energy: -5, rarity: 'common', unlockLevel: 1 },
+    puzzleBox: { id: 'puzzleBox', name: 'Puzzle Box', emoji: '🧩', cost: 30, happiness: 15, intelligence: 10, energy: -5, rarity: 'uncommon', unlockLevel: 3 },
+    rubberDuck: { id: 'rubberDuck', name: 'Rubber Duck', emoji: '🐤', cost: 10, happiness: 20, energy: -5, rarity: 'common', unlockLevel: 1 },
+    iceBlock: { id: 'iceBlock', name: 'Ice Block', emoji: '🧊', cost: 20, happiness: 25, energy: -10, rarity: 'uncommon', unlockLevel: 4 },
+    gem: { id: 'gem', name: 'Shiny Gem', emoji: '💎', cost: 100, happiness: 50, intelligence: 5, rarity: 'rare', unlockLevel: 10 },
+    laserPointer: { id: 'laserPointer', name: 'Laser Pointer', emoji: '🔴', cost: 25, happiness: 35, energy: -15, rarity: 'uncommon', unlockLevel: 5 },
+    featherWand: { id: 'featherWand', name: 'Feather Wand', emoji: '🪶', cost: 18, happiness: 30, energy: -10, rarity: 'common', unlockLevel: 2 },
+    squeakyToy: { id: 'squeakyToy', name: 'Squeaky Toy', emoji: '🧸', cost: 15, happiness: 25, energy: -5, rarity: 'common', unlockLevel: 1 },
+    frisbee: { id: 'frisbee', name: 'Frisbee', emoji: '🥏', cost: 20, happiness: 30, energy: -15, strength: 5, rarity: 'common', unlockLevel: 2 }
+};
